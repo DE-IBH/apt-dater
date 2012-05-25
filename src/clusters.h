317d5b2ad042016c8e995d22d80d22ecc2f71b96
Thomas Liske <thomas@fiasko-nw.net>
2012-05-25 16:48:30 +0200

    Drop locking stuff - must be done in the command handler.

--- src/clusters.h
+++ src/clusters.h
@@ -30,11 +30,7 @@
 
 #ifdef FEAT_CLUSTERS
 
-void cluster_host_reset(HostNode *);
 void cluster_host_add(HostNode *, const gchar *);
-
-gboolean cluster_host_acquire(HostNode *);
-void cluster_host_release(HostNode *);
 
 #endif /* FEAT_CLUSTERS */
 
