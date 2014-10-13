28e83bf9279d7d80dc0f611fc3aa613ba6e5e6ad
Thomas Liske <thomas@fiasko-nw.net>
2014-10-13 22:47:46 +0200

    Supply default config files, again.

--- src/keyfiles.c
+++ src/keyfiles.c
@@ -35,103 +35,32 @@
 
 #include <libconfig.h>
 
-static char apt_dater_conf[] = "# Config file of apt-dater in the form of the"
- " glib GKeyFile required\n\n[Paths]\n# Default: $XDG_CONFIG_HOME/apt-dater/h"
- "osts.conf\n#HostsFile=path-to/hosts.conf\n\n# Default: $XDG_DATA_HOME/apt-d"
- "ater\n#StatsDir=path-to/stats\n\n[SSH]\n# SSH binary\nCmd=/usr/bin/ssh\nOpt"
- "ionalCmdFlags=-t\n\n# SFTP binary\nSFTPCmd=/usr/bin/sftp\n\n"
- "#[Screen]\n## Default: $XDG_CONFIG_HOME"
- "/apt-dater/screenrc\n#RCFile=path-to/screenrc\n#\n## Default: %m # %u@%h:%p"
- "\n#Title=%m # %U%H\n#\n\n# These values requires apt-dater-host to be i"
- "nstalled on the target host.\n# You could call apt/aptitude directly (see a"
- "pt-dater-host source),\n# but this is not recommended.\n[Commands]\nCmdRefr"
- "esh=apt-dater-host refresh\nCmdUpgrade=apt-dater-host upgrade\nCmdInstall=a"
- "pt-dater-host install %s\n\n[Appearance]\n# Colors      = (COMPONENT FG BG "
- "\';\')*\n# COMPONENT ::= \'default\' | \'menu\' | \'status\' | \'selector\'"
- " | \'hoststatus\' |\n#               \'query\' | \'input\'\n# FG        ::="
- " COLOR\n# BG        ::= COLOR\n# COLOR     ::= \'black\' | \'blue\' | \'cya"
- "n\' | \'green\' | \'magenta\' | \'red\' |\n#               \'white\' | \'ye"
- "llow\'\nColors=menu brightgreen blue;status brightgreen blue;selector black"
- " red;\n\n#[TCLFilter]\n#Predefined filter expression on startup.\n#FilterEx"
- "p=return [expr [string compare $lsb_distri \"Debian\"] == 0 && $lsb_rel < 5"
- ".0]\n#Load this TCL file on startup in the same context as the FilterExp.\n"
- "#FilterFile=/patch/to/file.tcl\n";
+EXTLD(apt_dater_config);
+EXTLD(hosts_config);
+EXTLD(screenrc);
 
-static char hosts_conf[] = "# Syntax:\n#\n#  [Customer Name]\n#  Hosts=([Opti"
- "onalUser@]host.domain[:OptionalPort];)*\n#\n\n[Localdomain]\nHosts=localhos"
- "t;\n\n[IBH]\nHosts=example1.ibh.net;example2.ibh.net;test@example3.ibh.net:"
- "62222;\n";
-
-static char screenrc[] = "# -------------------------------------------------"
- "-----------------------------\n# SCREEN SETTINGS\n# -----------------------"
- "-------------------------------------------------------\n\nstartup_message "
- "off\n\n#defflow on # will force screen to process ^S/^Q\ndeflogin on\nautod"
- "etach on\n\n# turn visual bell on\nvbell on\n\n# define a bigger scrollback"
- ", default is 100 lines\ndefscrollback 2048\n\n# ---------------------------"
- "---------------------------------------------------\n# SCREEN KEYBINDINGS\n"
- "# -------------------------------------------------------------------------"
- "-----\n\n# Remove some stupid / dangerous key bindings\nbind ^k\n#bind L\nb"
- "ind ^\\\n# Make them better\nbind \\\\ quit\nbind K kill\nbind I login on\n"
- "bind O login off\nbind } history\n\n# Sessions should stay until destroyed "
- "by pressing q\nzombie \'q\'\n\n# --------------------------------------"
- "----------------------------------------\n# TERMINAL SETTINGS\n# ----------"
- "--------------------------------------------------------------------\n\n# T"
- "he vt100 description does not mention \"dl\". *sigh*\ntermcapinfo vt100 dl="
- "5\\E[M\n\n# Set the hardstatus prop on gui terms to set the titlebar/icon t"
- "itle\ntermcapinfo xterm*|rxvt*|kterm*|Eterm* hs:ts=\\E]0;:fs=\\007:ds=\\E]0"
- ";\\007:OP\n\n# set these terminals up to be \'optimal\' instead of vt100\n#"
- "termcapinfo xterm*|linux*|rxvt*|Eterm* OP\n\n# Change the xterm initializat"
- "ion string from is2=\\E[!p\\E[?3;4l\\E[4l\\E>\n# (This fixes the \"Aborted "
- "because of window size change\" konsole symptoms found\n#  in bug #134198)"
- "\ntermcapinfo xterm \'is=\\E[r\\E[m\\E[2J\\E[H\\E[?7h\\E[?1;4;6l\'\n\n# To "
- "get screen to add lines to xterm\'s scrollback buffer, uncomment the\n# fol"
- "lowing termcapinfo line which tells xterm to use the normal screen buffer\n"
- "# (which has scrollback), not the alternate screen buffer.\n#\ntermcapinfo "
- "xterm|xterms|xs|rxvt ti@:te@\n\n# Add caption line with clock, window title"
- " and window flags.\ncaption always \"%{b bG}%c%=%t%=%f\"\n\n# Catch zmodem "
- "file transfers\nzmodem catch\n";
-
-
-int chkForInitialConfig(const gchar *cfgdir, const gchar *cfgfile)
-{
- FILE *fp = NULL;
- gchar *pathtofile = NULL;
-
- if(g_file_test (cfgfile, G_FILE_TEST_IS_REGULAR|G_FILE_TEST_EXISTS) == FALSE) {
-  if(g_file_test(cfgdir, G_FILE_TEST_IS_DIR) == FALSE) {
-   if(g_mkdir_with_parents (cfgdir, 0700)) return(1);
-
-   /* Create a example hosts.conf */
-   pathtofile = g_strdup_printf("%s/hosts.conf", cfgdir);
-   if(!pathtofile) g_error(_("Out of memory."));
-   fp = fopen(pathtofile, "wx");
-   g_message(_("Creating file %s"), pathtofile);
-   if(fp) {
-    if(fwrite(hosts_conf, sizeof(hosts_conf)-1, 1, fp) != 1) g_error(_("Could not write to file %s."), pathtofile);
-    fclose(fp);
-   }
-   g_free(pathtofile);
-
-   /* Create a the example screenrc */
-   pathtofile = g_strdup_printf("%s/screenrc", cfgdir);
-   if(!pathtofile) g_error(_("Out of memory."));
-   fp = fopen(pathtofile, "wx");
-   g_message(_("Creating file %s"), pathtofile);
-   if(fp) {
-    if(fwrite(screenrc, sizeof(screenrc)-1, 1, fp) != 1) g_error(_("Could not write to file %s."), pathtofile);
-    fclose(fp);
-   }
-   g_free(pathtofile);
-
-   /* Create initial config file */
-   fp = fopen(cfgfile, "wx");
-   if(!fp) return(1);
-
-   g_message(_("Creating file %s"), cfgfile);
-   if(fwrite(apt_dater_conf, sizeof(apt_dater_conf)-1, 1, fp) != 1) g_error(_("Could not write to file %s."), cfgfile);
-   fclose(fp);
+#define DUMP_CONFIG(fn, bin)						\
+  if(g_file_test (fn, G_FILE_TEST_IS_REGULAR|G_FILE_TEST_EXISTS) == FALSE) { \
+     FILE *fp = NULL; \
+     gchar *pathtofile = g_strdup_printf("%s/%s", cfgdir, (fn));	\
+     if(!pathtofile) g_error(_("Out of memory.")); \
+     fp = fopen(pathtofile, "wx");		     \
+     g_message(_("Creating default config file %s"), pathtofile);   \
+     if(fp) {								\
+       if(fwrite(LDVAR(bin), LDLEN(bin), 1, fp) != 1) g_error(_("Could not write to file %s."), pathtofile); \
+       fclose(fp);							\
+     }									\
+     g_free(pathtofile); \
   }
- }
+
+int chkForInitialConfig(const gchar *cfgdir, const gchar *cfgfile) {
+  if(g_file_test(cfgdir, G_FILE_TEST_IS_DIR) == FALSE) {
+    if(g_mkdir_with_parents (cfgdir, 0700)) return(1);
+  }
+  
+  DUMP_CONFIG("apt-dater.config", apt_dater_config);
+  DUMP_CONFIG("hosts.config", hosts_config);
+  DUMP_CONFIG("screenrc", screenrc);
 
  return(0);
 }
--- src/keyfiles.h
+++ src/keyfiles.h
@@ -32,4 +32,34 @@ gboolean loadConfig (char *filename, CfgFile *);
 void freeConfig (CfgFile *cfg);
 int chkForInitialConfig(const gchar *, const gchar *);
 
+#ifdef __APPLE__ /* OSX */
+
+#include <mach-o/getsect.h>
+#define EXTLD(NAME) \
+  extern const unsigned char _section$__DATA__ ## NAME [];
+#define LDVAR(NAME) _section$__DATA__ ## NAME
+#define LDLEN(NAME) (getsectbyname("__DATA", "__" #NAME)->size)
+
+#elif (defined __WIN32__)  /* mingw */
+
+#define EXTLD(NAME) \
+  extern const unsigned char binary_ ## NAME ## _start[]; \
+  extern const unsigned char binary_ ## NAME ## _end[];
+#define LDVAR(NAME) \
+  binary_ ## NAME ## _start
+#define LDLEN(NAME) \
+  ((binary_ ## NAME ## _end) - (binary_ ## NAME ## _start))
+
+#else /* assume gnu/linux ld */
+
+#define EXTLD(NAME) \
+  extern const unsigned char _binary_ ## NAME ## _start[]; \
+  extern const unsigned char _binary_ ## NAME ## _end[];
+#define LDVAR(NAME) \
+  _binary_ ## NAME ## _start
+#define LDLEN(NAME) \
+  ((_binary_ ## NAME ## _end) - (_binary_ ## NAME ## _start))
+#endif
+
 #endif /* _KEYFILES_H */
+
