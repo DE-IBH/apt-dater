66ee892e245329de3feb34cc0f6051a82c04440e
Mike Beattie <mike@ethernal.org>
2020-02-24 04:02:01 +0000

    Update travis-before.sh - new tcl dev package
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- build/travis-before.sh
+++ build/travis-before.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)/..
 
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y automake gettext libpopt-dev libglib2.0-dev libncursesw5-dev tcl8.5-dev libxml2-dev libconfig-dev screen tmux xxd
+apt-get install -y automake gettext libpopt-dev libglib2.0-dev libncursesw5-dev tcl8.6-dev libxml2-dev libconfig-dev screen tmux xxd
