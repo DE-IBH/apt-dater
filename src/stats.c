7c9a8ef10a3859e93ad3a577600f99086be5bffe
Stefan Bühler <source@stbuehler.de>
2024-10-04 01:39:59 +0200

    sscanf: remove wrong (too short) and unnecessary zero termination of uuid field

--- src/stats.c
+++ src/stats.c
@@ -414,8 +414,6 @@ gboolean getUpdatesFromStat(HostNode *n)
    continue;
 
   if (sscanf((gchar *) line, "UUID: %" QUOTE(UUID_STRLEN) "s", n->uuid)) {
-   n->uuid[UUID_STRLEN-1] = 0;
-
    linesok++;
    continue;
   }
